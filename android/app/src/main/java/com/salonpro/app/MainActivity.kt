346fb1e724cfb3df5c666cf931e5a16e16bea56a
gopiminhas854-coder <gopiminhas854@gmail.com>
2026-09-23 11:30:17 +0530

    Connect Android wrapper to Salon Pro

---
+++ android/app/src/main/java/com/salonpro/app/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.salonpro.app
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+private const val SALON_PRO_URL = "https://salon-pro.onrender.com/"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        webView = WebView(this).apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.databaseEnabled = true
+            settings.setSupportZoom(false)
+            webViewClient = WebViewClient()
+            loadUrl(SALON_PRO_URL)
+        }
+        setContentView(webView)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
+    }
+}
