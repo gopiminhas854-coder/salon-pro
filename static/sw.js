3a7f16d803a5f6978b25ab2d35a191f6c4212c79
gopiminhas854-coder <gopiminhas854@gmail.com>
2026-09-23 11:07:01 +0530

    Bust stale service worker cache after UI fixes

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "salon-pro-static-v1";
+const CACHE_NAME = "salon-pro-static-v2";
 const STATIC_ASSETS = ["/static/css/style.css"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS)));
